aecd4e29b33c37423a5824bcfda2c0de5802ef6e
공수정 <0sujeong@nectarsoft.co.kr>
2022-05-31 12:26:53 +0900

    회원 관련 레이아웃 적용

--- src/main/java/kr/co/nectarsoft/community/user/controller/LoginConroller.java
+++ src/main/java/kr/co/nectarsoft/community/user/controller/LoginConroller.java
@@ -1,29 +1,30 @@
 package kr.co.nectarsoft.community.user.controller;
 
+import kr.co.nectarsoft.community.user.vo.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
+ * description    : 로그인 관련 컨트롤러
  * packageName    : kr.co.nectarsoft.community.user
  * fileName       : LoginConroller
  * author         : GongSuJeong
  * date           : 2022-05-27
- * description    : 로그인 관련 컨트롤러
  * ===========================================================
  * DATE              AUTHOR             NOTE
  * -----------------------------------------------------------
  * 2022-05-27        GongSuJeong       최초 생성
  */
 
-@RequestMapping("/user/*")
+@RequestMapping("/login/*")
 @Controller
 public class LoginConroller {
 
-
-
-    @GetMapping("login.do")
-    public String joinForm(){
+    @GetMapping("form.do")
+    public String joinForm(Model model){
+        model.addAttribute("user", new User());
         return "/user/login";
     }
 }
